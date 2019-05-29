f9ee1b5837eacf878b4e7e2d35bb62793f587609
FeatMish <50726907+FeatMish@users.noreply.github.com>
2019-05-29 22:38:14 +1000

    Junit Fix

--- untitled2/src/TestJunit.java
+++ untitled2/src/TestJunit.java
@@ -5,12 +5,10 @@ import java.awt.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestJunit {
-
-
-
+    
     @Test
     public void testRectangleX() {
-        //new Rectangle(1,2,3,4,false, Color.WHITE,Color.WHITE);
-        //assertEquals(1, Rectangle.getXo());
+       Rectangle rect = new Rectangle(1,2,3,4,false, Color.WHITE,Color.WHITE);
+        assertEquals(1, rect.getXo());
     }
 }
